df6cf7f11fee21cb3e4ebfda8fd76a040729817b
Thiago Koch <tkoch.dev@gmail.com>
2023-04-13 22:37:48 -0300

    feat: add users.routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
+const routes = require("./routes");
+
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 const PORT = 3000;
 
-app.post("/user", (request, response) => { 
-  const {name, email, password} = request.body;
-  response.json({ name, email, password }) // o response.json serve para retornarmos uma resposta do tipo json.
-})
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
