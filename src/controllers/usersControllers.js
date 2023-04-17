9a1d11807afc3c115e382821548d84fd6d8bb56e
Thiago Koch <tkoch.dev@gmail.com>
2023-04-16 21:34:02 -0300

    feat: add usersControllers

---
+++ src/controllers/usersControllers.js
@@ -0,0 +1,8 @@
+class UsersControllers {
+  create(request, response) {
+    const {name, email, password} = request.body;
+    response.json({ name, email, password }) // o response.json serve para retornarmos uma resposta do tipo json.
+  }
+}
+
+module.exports = UsersControllers;
