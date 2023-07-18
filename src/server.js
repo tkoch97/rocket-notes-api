4b685909e2b663b44eae6ba8faee345c6d1b8277
Thiago Koch <tkoch.dev@gmail.com>
2023-07-18 18:42:42 -0300

    feat: create new request for visualize user img

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 require("express-async-errors");
-const express = require('express');
 const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload");
+
+const cors = require("cors");
+const express = require('express');
 const routes = require("./routes");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 migrationsRun();
 
